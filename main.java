6c728cae9b076942ac5fde9aafc3493c7de15a42
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-08-02 19:21:59 +0530

    changes

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void Main ( String[] args ) {
+        int a=30;
         System.out.println("Welcome to our Project");
         System.out.println("My name is Dhruv");
         System.out.println("HI");
@@ -7,5 +8,8 @@ public class main {
         System.out.println("Cycle is good");
         System.out.println("Rajib Mondal");
         System.out.println("hitansh gupta");
+        System.out.println("thats ittttt!!!!");
+        System.out.println("lalalalala");
+        System.out.println(a);
     }
 }
